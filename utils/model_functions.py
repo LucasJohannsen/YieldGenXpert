3f6a20e261be56509b22fbcdfbdb6d12e1744a8f
Lucas <lucas-johannsen@t-online.de>
2024-05-23 18:56:26 +0200

    Update model_functions.py
    to pandas

--- utils/model_functions.py
+++ utils/model_functions.py
@@ -59,7 +59,7 @@ def rf(mode, best_param, X_train, X_test, y_train, y_test):
         # Fit the model
         model.fit(X_train_csr, y_train)
         # Predict the values
-        y_pred = model.predict(X_test_csr)
+        y_pred = model.predict(X_test_csr).to_pandas()
         # Calculate the mean squared error
         mse = mean_squared_error(y_test, y_pred)
         # Calculate the R-squared value
